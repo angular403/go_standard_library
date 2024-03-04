5a58c0e76dc590ec494a3f82c72e11cafbc3ad25
Andrew Napitupulu <angularsprings403@gmail.com>
2024-03-04 10:34:19 +0700

    belajar package golang

---
+++ fmt.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstname := "Andrew"
+	lastName := "Wiliam"
+
+	fmt.Printf("Hello '%s %s' \n", firstname, lastName)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module belajar_golang_standard_library
+
+go 1.20
