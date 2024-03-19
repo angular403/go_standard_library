e4d67cb53462ffad14d3e1a967472a96fda954dc
Andrew Napitupulu <angularsprings403@gmail.com>
2024-03-19 14:54:28 +0700

    finish belajar golang standard library

---
+++ bufio_write.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+func main() {
+	writer := bufio.NewWriter(os.Stdout)
+	_, _ = writer.WriteString("Hello World\n")
+	_, _ = writer.WriteString("Selamat Datang\n")
+	writer.Flush()
+}
